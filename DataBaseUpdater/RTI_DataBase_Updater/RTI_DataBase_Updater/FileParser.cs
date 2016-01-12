3f5d5a6faa8b8ae798aef1cd3c90f7b1ff3ee993
John August <jpa52@drexel.edu>
2016-01-12 01:21:20 -0500

    Modified File Reader Class
    Added functionality to extract tab delimited data from each file.

--- DataBaseUpdater/RTI_DataBase_Updater/RTI_DataBase_Updater/FileParser.cs
+++ DataBaseUpdater/RTI_DataBase_Updater/RTI_DataBase_Updater/FileParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace RTI.Database.Updater
@@ -26,12 +27,12 @@ namespace RTI.Database.Updater
         private void OpenFile(string filePath)
         {
             try
-            {   // Open the text file using a stream reader.
+            {
+                // Open the text file using a stream reader.
                 using (StreamReader sr = new StreamReader(filePath))
                 {
-                    // Read the stream to a string, and write the string to the console.
-                    String line = sr.ReadToEnd();
-                    Console.WriteLine(line);
+                    // Read the stream to a string, and write the string to the console
+                    extractData(sr);
                 }
             }
             catch (Exception e)
@@ -43,15 +44,61 @@ namespace RTI.Database.Updater
 
 
         /// <summary>
-        /// 
+        /// Extracts conductivity and date values from each line
         /// </summary>
         /// <returns></returns>
-        public void parseLine (string fileContents)
+        public void extractData (StreamReader fileContents)
         {
+            Dictionary<DateTime, int> conductivity = new Dictionary<DateTime, int>();
+
             DateTime date;
-            double conductivityValue; 
+            double cond;
+            char[] delimiter = new char[] { '\t' };
+            int dateCol = 0, condCol = 0, numHeaders = 2;
 
 
+            try {
+                // Read the file line by line 
+                while (!fileContents.EndOfStream)
+                {
+                    string line = fileContents.ReadLine();
+
+                    // Extract Data
+                    if (!line.StartsWith("#"))
+                    {
+                        if (!line.Contains("agency_cd") || !line.Contains("site_no")  || !line.Contains("datetime"))
+                        {
+                            var segments = line.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+                            date = Convert.ToDateTime(segments[dateCol]);
+                            cond = Convert.ToDouble(segments[condCol]);
+
+                            //DEBUG
+                            Console.WriteLine(date + " " + cond);
+                        }
+                        else 
+                        {
+                            var header = line.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+                            dateCol = header.Select((v, i) => new { Index = i, Value = v }).Where(p => p.Value == "datetime").Select(p => p.Index).ToList().First();
+                            condCol = header.Select((v, i) => new { Index = i, Value = v }).Where(p => p.Value.Contains("00095") && !p.Value.Contains("cd")).Select(p => p.Index).ToList().First();
+
+                            for (int i = 0; i < numHeaders-1; i++)
+                                fileContents.ReadLine();
+               
+                            //dateCol = headerList.IndexOf("datetime");
+                            
+                        }       
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                fileContents.Dispose();
+            }
+            
         }
 
     public Dictionary<DateTime, double> cond { get; set; }
