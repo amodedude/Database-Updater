a262493e81cb5a1e0d05b5e84e0083ad5615a64d
John August <jpa52@drexel.edu>
2016-01-12 01:48:06 -0500

    Started the Uploader Class
    Began work on the Uploader class to upload extracted data to the RTI
    database.

--- DataBaseUpdater/RTI_DataBase_Updater/RTI_DataBase_Updater/FileParser.cs
+++ DataBaseUpdater/RTI_DataBase_Updater/RTI_DataBase_Updater/FileParser.cs
@@ -32,7 +32,9 @@ namespace RTI.Database.Updater
                 using (StreamReader sr = new StreamReader(filePath))
                 {
                     // Read the stream to a string, and write the string to the console
-                    extractData(sr);
+                    var data = extractData(sr);
+                    Uploader rtiUploader = new Uploader();
+                    rtiUploader.beginUpload(data);
                 }
             }
             catch (Exception e)
@@ -44,15 +46,18 @@ namespace RTI.Database.Updater
 
 
         /// <summary>
-        /// Extracts conductivity and date values from each line
+        /// Extracts conductivity and date values from each line.
+        /// Returns a list of data records for each day. 
         /// </summary>
+        /// <param name="fileContents"></param>
         /// <returns></returns>
-        public void extractData (StreamReader fileContents)
+        public List<WaterData> extractData (StreamReader fileContents)
         {
             Dictionary<DateTime, int> conductivity = new Dictionary<DateTime, int>();
 
             DateTime date;
             double cond;
+            List <WaterData> data = new List<WaterData>();
             char[] delimiter = new char[] { '\t' };
             int dateCol = 0, condCol = 0, numHeaders = 2;
 
@@ -66,29 +71,33 @@ namespace RTI.Database.Updater
                     // Extract Data
                     if (!line.StartsWith("#"))
                     {
-                        if (!line.Contains("agency_cd") || !line.Contains("site_no")  || !line.Contains("datetime"))
+                        if (!line.Contains("agency_cd") || !line.Contains("site_no") || !line.Contains("datetime"))
                         {
                             var segments = line.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
                             date = Convert.ToDateTime(segments[dateCol]);
                             cond = Convert.ToDouble(segments[condCol]);
 
+                            var todaysData = new WaterData();
+                            todaysData.date = date;
+                            todaysData.waterConductivity = cond;
+
+                            data.Add(todaysData);
+
                             //DEBUG
                             Console.WriteLine(date + " " + cond);
                         }
-                        else 
+                        else
                         {
                             var header = line.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
                             dateCol = header.Select((v, i) => new { Index = i, Value = v }).Where(p => p.Value == "datetime").Select(p => p.Index).ToList().First();
                             condCol = header.Select((v, i) => new { Index = i, Value = v }).Where(p => p.Value.Contains("00095") && !p.Value.Contains("cd")).Select(p => p.Index).ToList().First();
 
-                            for (int i = 0; i < numHeaders-1; i++)
+                            for (int i = 0; i < numHeaders - 1; i++)
                                 fileContents.ReadLine();
-               
-                            //dateCol = headerList.IndexOf("datetime");
-                            
-                        }       
+                        }
                     }
                 }
+                return data;
             }
             catch(Exception ex)
             {
@@ -97,12 +106,17 @@ namespace RTI.Database.Updater
             finally
             {
                 fileContents.Dispose();
-            }
-            
+            }    
         }
+    }
 
-    public Dictionary<DateTime, double> cond { get; set; }
-
-
-}
+    /// <summary>
+    /// Used to store water data durring the file reading process. 
+    /// </summary>
+    class WaterData
+    {
+        public DateTime date { get; set; }
+        public double waterConductivity { get; set; }
+    }
+    
 }
---
+++ DataBaseUpdater/RTI_DataBase_Updater/RTI_DataBase_Updater/Uploader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTI.Database.Updater
+{
+    /// <summary>
+    /// Uploads retrieved data into the RTI database. 
+    /// </summary>
+    class Uploader
+    {
+        public void beginUpload(List<WaterData> data)
+        {
+            foreach (var instant in data)
+            {
+               //TODO - Add data to the RTI DB here~!
+            }
+        }
+    }
+}
